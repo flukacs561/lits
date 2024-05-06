bca44ebd1df7905a81ddfdd430d9f67499d24adf
Ferenc Lukács <flukacs@proton.me>
2024-05-06 23:52:45 +0200

    refactoring and core commands
    - got rid of Maybe [Book] types
    - resolved resource locked issue
    - implemented: import, clean and remove-dups

--- lib/Commands.hs
+++ lib/Commands.hs
@@ -5,12 +5,16 @@ module Commands
     doAdd,
     doDelete,
     doInit,
+    doImport,
+    doRemoveDups,
+    doClean,
   )
 where
 
-import DataBase (Book, writeToDataBase)
-import EntryManager (prepareNewEntry, removeEntry)
-import FileManager (createDBFile, deleteFile)
+import Data.List (nub)
+import DataBase (Book, fileName, writeToDataBase)
+import EntryManager (prepareNewEntry, removeEntry, runCleanCommand, runImportCommand)
+import FileManager (createDBFile, removeFile)
 import Filter (runFilterCmd)
 import Formatting (printBooks)
 
@@ -21,28 +25,46 @@ doInit :: [String] -> IO ()
 doInit [] = createDBFile
 doInit _ = error "Too many arguments."
 
-doList :: [String] -> Maybe [Book] -> IO ()
+doList :: [String] -> [Book] -> IO ()
 doList [] = printBooks
 doList _ = error "Too many arguments."
 
 -- Input validation happend in `parseFilterInput`.
-doFilter :: [String] -> Maybe [Book] -> IO ()
-doFilter args db = printBooks $ runFilterCmd args <$> db
+doFilter :: [String] -> [Book] -> IO ()
+doFilter args db = printBooks $ runFilterCmd args db
 
-doAdd :: [String] -> Maybe [Book] -> IO ()
-doAdd args db = do
-  newBook <- prepareNewEntry' args
-  writeToDataBase $ fmap (newBook :) db
-  where
-    prepareNewEntry' [] = error "No file specified"
-    prepareNewEntry' [path] = prepareNewEntry path
-    prepareNewEntry' _ = error "Too many arguments"
+doAdd :: [String] -> [Book] -> IO ()
+doAdd [] _ = error "No file specified."
+doAdd [file] db =
+  if hasFileEntry file db
+    then error "This file already has an entry in the database."
+    else do
+      newBook <- prepareNewEntry file
+      writeToDataBase (newBook : db)
+doAdd _ _ = error "Too many arguments."
 
-doDelete :: [String] -> Maybe [Book] -> IO ()
-doDelete args db = do
-  writeToDataBase $ removeEntry' args db
-  deleteFile args
-  where
-    removeEntry' [] = error "No file specified."
-    removeEntry' [file] = removeEntry file
-    removeEntry' _ = error "Too many arguments."
+hasFileEntry :: FilePath -> [Book] -> Bool
+hasFileEntry file = any (\book -> fileName book == file)
+
+doDelete :: [String] -> [Book] -> IO ()
+doDelete [] _ = error "No file specified."
+doDelete [file] db = do
+  writeToDataBase $ removeEntry file db
+  removeFile file
+doDelete _ _ = error "Too many arguments."
+
+doImport :: [String] -> [Book] -> IO ()
+doImport [] db = do
+  newDB <- runImportCommand db
+  writeToDataBase newDB
+doImport _ _ = error "Too many arguments."
+
+doRemoveDups :: [String] -> [Book] -> IO ()
+doRemoveDups [] = writeToDataBase . nub
+doRemoveDups _ = error "Too many arguments."
+
+doClean :: [String] -> [Book] -> IO ()
+doClean [] db = do
+  newDB <- runCleanCommand db
+  writeToDataBase newDB
+doClean _ _ = error "Too many arguments."
