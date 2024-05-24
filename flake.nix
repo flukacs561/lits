f1484c1d9d4733023f4ac1ec2c9698e42c109aad
Ferenc Lukács <flukacs@proton.me>
2024-05-24 10:15:42 +0200

    add nix flake support

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "LiTS is a library tagging CLI tool";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+  outputs = { self, nixpkgs, ... }:
+  let system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    packages.${system}.default = pkgs.haskellPackages.mkDerivation {
+      pname = "lits";
+      version = "0.1.0.0";
+      src = ./.;
+      isLibrary = false;
+      isExecutable = true;
+      buildDepends = with pkgs.haskellPackages; [
+        base
+        aeson
+        bytestring
+        directory
+        containers
+        filepath
+        text
+        tasty
+        tasty-hunit
+      ];
+      executableHaskellDepends = with pkgs.haskellPackages; [ base ];
+      description = "Library Tagging System";
+      license = pkgs.lib.licenses.mit;
+      mainProgram = "lits";
+    };
+  };
+}
